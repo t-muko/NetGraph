618311b2e281362bc361a98969d120d7c6d8d2ba
Teemu Koivisto <teemuko@iki.fi>
2017-01-20 08:05:56 +1300

    Create Graphviz.py

---
+++ Graphviz.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 22:03:21 2017
+
+@author: teemuko
+"""
+
+import networkx as nx
+
+import matplotlib.pyplot as plt
+#from networkx.drawing.nx_pydot import write_dot
+from networkx.drawing.nx_agraph import write_dot
+
+#G=nx.Graph()
+#G.add_nodes_from([2,3,'fooba','mollo'])
+#G.add_edge(1,2)
+
+MG=nx.MultiDiGraph()
+MG.add_nodes_from([1,2,3],label='baa')
+MG.add_weighted_edges_from([(1,2,0.5), (2,1,0.5), (2,3,.5)],label="foo")
+MG.degree(weight='weight')
+
+#nx.draw(MG)
+#nx.draw(MG,pos=nx.spring_layout(MG)) # use spring layout
+#nx.draw_networkx(MG,labels={'label'})
+
+#write_dot(MG,'multi.dot')
+nx.draw_graphviz(MG, prog='neato')
+
